5b753ba3d163af7452d96dac0c98a597d34bd202
Michael Keller <mk@viu.ch>
2019-10-20 14:17:09 +0200

    Implement feedback of @zVolodymyr

--- Harvest.Api/Models/IdNameModel.cs
+++ Harvest.Api/Models/IdNameModel.cs
@@ -6,7 +6,7 @@ namespace Harvest.Api
 {
     public class IdNameModel
     {
-        public long? Id { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
     }
 }
