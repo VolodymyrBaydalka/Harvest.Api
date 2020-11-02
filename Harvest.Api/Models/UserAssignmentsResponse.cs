093e4417cf2d725b9d34891ea2ec493bb0415bab
zhezhangjohn <zhe.zhang.john@gmail.com>
2020-11-02 20:41:30 +1300

    Fix issue: Update project parameter fail; Add API for listing user assignment; Add parameter 'is_active' for listing user and project

---
+++ Harvest.Api/Models/UserAssignmentsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harvest.Api
+{
+    public class UserAssignmentsResponse : PagedList
+    {
+        public UserAssignment[] UserAssignments { get; set; }
+    }
+}
