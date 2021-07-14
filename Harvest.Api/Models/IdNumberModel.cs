16c42ac49adeccf41ca63d6cb0620fc7b1375ea7
zhezhangjohn <zhe.zhang.john@gmail.com>
2021-07-14 12:18:50 +1200

    Add Invoice object into Time Entry

---
+++ Harvest.Api/Models/IdNumberModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Harvest.Api
+{
+    [DebuggerDisplay("{Number}")]
+    public class IdNumberModel
+    {
+        public long Id { get; set; }
+        public string Number { get; set; }
+    }
+}
