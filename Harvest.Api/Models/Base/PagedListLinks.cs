8bb0d6cfdd7cb112c777c5a5fc1a70a5a80402ef
zVolodymyr <volodymyr.baydalka@gmail.com>
2021-08-24 22:01:46 +0300

    fixed Last property Added new attribute to mark undocumented API

--- Harvest.Api/Models/Base/PagedListLinks.cs
+++ Harvest.Api/Models/Base/PagedListLinks.cs
@@ -9,6 +9,6 @@ namespace Harvest.Api
         public string First { get; set; }
         public string Next { get; set; }
         public string Previous { get; set; }
-        public string Past { get; set; }
+        public string Last { get; set; }
     }
 }
